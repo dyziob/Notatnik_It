340b34bfddeba69d3adde1c8e57148466787b4cf
dyziob <dyziob65@gmail.com>
2025-12-16 01:20:40 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
+import LoginPage from './components/index';
 import RegistrationPage from './components/RegistrationPage';
 import Main from './components/main';
 import PersonalDetails from './components/PersonalDetails';
